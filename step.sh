7b45e1d838a2810149ec08b13cfc33bc843e0062
DamienBitrise <60897705+DamienBitrise@users.noreply.github.com>
2022-06-14 18:16:51 -0700

    Install Dep for macOS

--- step.sh
+++ step.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+git clone https://github.com/breiter/vpnc.git
+cd vpnc
+make
+sudo make install
+
 if [[ ! -z ${config_file_content} ]]; then
-  echo "${config_file_content}" > /etc/vpnc/default.conf
+  sudo echo "${config_file_content}" > /etc/vpnc/default.conf
 fi
 
 set -- ${commandline_options}
