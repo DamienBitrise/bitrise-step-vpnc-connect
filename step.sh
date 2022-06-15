dc5ee441851eb940da2d49bdec740b6792ce240e
DamienBitrise <60897705+DamienBitrise@users.noreply.github.com>
2022-06-14 18:27:49 -0700

    chown

--- step.sh
+++ step.sh
@@ -5,6 +5,7 @@ git clone https://github.com/breiter/vpnc.git
 cd vpnc
 make
 sudo make install
+chown root /etc/vpnc/default.conf
 
 if [[ ! -z ${config_file_content} ]]; then
   sudo echo "${config_file_content}" > /etc/vpnc/default.conf
