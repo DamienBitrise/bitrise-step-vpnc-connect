3edc8092cc0e2af38900e3021c324db649087f85
DamienBitrise <60897705+DamienBitrise@users.noreply.github.com>
2022-06-14 18:38:09 -0700

    add sudo

--- step.sh
+++ step.sh
@@ -4,12 +4,12 @@ set -e
 git clone https://github.com/breiter/vpnc.git
 cd vpnc
 make
-sudo make install
 sudo su
+sudo make install
 
 if [[ ! -z ${config_file_content} ]]; then
   echo "${config_file_content}" | sudo tee -a /etc/vpnc/default.conf
 fi
 
 set -- ${commandline_options}
-vpnc "$@"
+sudo vpnc "$@"
